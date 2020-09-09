9cbe31f4189db1d960e1cddc373a04aaa4bc8a64
Thiha Set <thihaset@mail.adelphi.edu>
2020-09-09 18:16:50 -0400

    Additional clarification on comments + fixing some errors with spacing and syntax

--- temp.c
+++ temp.c
@@ -7,10 +7,11 @@ int main(){
     scanf("%f%c",&deg,&id);
 
     //check what the user-inputted temperature unit is (whether it was fahrenheit, celsius, or kelvin, and proceed accordingly)
+    //declare vars representing the different types
     float f, c, k;
     switch (id)
     {
-    //case 1: type = celcius
+    //case 1: type = Celcius
     case 'C':
     case 'c':
         //acknowledge the input:
@@ -27,10 +28,11 @@ int main(){
         //return 0 to signify success
         return 0;
         break;
+    //case 2: type = Kelvin
     case 'K':
     case 'k':
         //acknowledge the input:
-        printf("%0.2f K converts to:");
+        printf("%0.2f K converts to:\n",deg);
 
         //first convert to Celcius (Formula: K-273.15) and then print:
         c=deg-273.15;
@@ -40,10 +42,11 @@ int main(){
         f=1.8 *(deg-273.15) +32;
         printf("\t%0.2f F\n",f);
         break;
+    //case 3: type = Fahrenheit
     case 'F':
     case 'f':
         //acknowledge the input:
-        printf("%0.2f F converts to:");
+        printf("%0.2f F converts to:\n",deg);
 
         //first convert to Kelvin (Formula: ((F-32)/1.8)+273.15 and print:
         k=((deg-32)/1.8)+273.15;
